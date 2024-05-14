d55587cfc4116d2c0de68fe85df37a24f3e2ccbc
EllinFeh <elisonfelipe16@hotmail.com>
2024-05-14 17:34:44 -0300

    Nav bar - Init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,23 @@
 import type { Config } from "tailwindcss";
+const flowbite = require("flowbite-react/tailwind");
 
 const config: Config = {
   content: [
+    flowbite.content(),
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        black: '#09090B',
+        white: '#FFF6F3',
+        primary:'#34375A',
+        secundary: '#906F8D',
+        sechv:'#564254',
+        container: '#0F101C',
+       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -15,6 +25,11 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+    
+  ],
+
+  
 };
 export default config;
